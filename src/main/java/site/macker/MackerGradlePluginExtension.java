8a23a6ce17de9c1324905a656ccf656b5892ff5b
Andrej Albrecht <andrejalbrecht@gmx.de>
2016-11-19 20:51:12 +0100

    Gradle plugin extension extended with attributes.

--- src/main/java/site/macker/MackerGradlePluginExtension.java
+++ src/main/java/site/macker/MackerGradlePluginExtension.java
@@ -2,6 +2,9 @@ package site.macker;
 
 public class MackerGradlePluginExtension {
     private String info = "MackerGradlePlugin - http://www.macker.site/";
+    private boolean ignoreFailures = false;
+    //private FileCollection sourceFiles;
+    //private File configFile;
 
     public String getInfo() {
         return this.info;
@@ -10,4 +13,29 @@ public class MackerGradlePluginExtension {
     public void setInfo(String info) {
         this.info = info;
     }
+
+    public boolean getIgnoreFailures() {
+        return this.ignoreFailures;
+    }
+
+    public void setIgnoreFailures(boolean ignoreFailures) {
+        this.ignoreFailures = ignoreFailures;
+    }
+
+    /*
+    public FileCollection getSourceFiles() {
+        return this.sourceFiles;
+    }
+
+    public void setSourceFiles(FileCollection sourceFiles) {
+        this.sourceFiles = sourceFiles;
+    }
+
+    public File getConfigFile() {
+        return this.configFile;
+    }
+
+    public void setConfigFile(File configFile) {
+        this.configFile = configFile;
+    }*/
 }
--- src/main/java/site/macker/MackerGradleTask.java
+++ src/main/java/site/macker/MackerGradleTask.java
@@ -1,9 +1,15 @@
 package site.macker;
 
+import java.io.File;
+
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.file.FileCollection;
+
 
 public class MackerGradleTask extends DefaultTask {
+    String info = "MackerGradleTask info";
+
     @TaskAction
     public void printInfo() {
         MackerGradlePluginExtension extension = getProject().getExtensions().findByType(MackerGradlePluginExtension.class);
@@ -11,6 +17,10 @@ public class MackerGradleTask extends DefaultTask {
             extension = new MackerGradlePluginExtension();
         }
         String info = extension.getInfo();
-        System.out.println(info);
+        boolean ignoreFailures = extension.getIgnoreFailures();
+        //File configFile = extension.getConfigFile();
+        //FileCollection sourceFiles = extension.getSourceFiles();
+        System.out.println("info:"+info);
+        System.out.println("ignoreFailures:"+ignoreFailures);
     }
 }
